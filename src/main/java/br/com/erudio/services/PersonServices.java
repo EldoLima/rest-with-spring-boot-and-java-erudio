b0af657130a8fa9b0a28e38feb8e689d17776ab0
Eldo Lima <eldosilva906@gmail.com>
2025-03-29 16:39:16 -0300

    Config log

--- src/main/java/br/com/erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -7,15 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 //operações para cadastrar um pessoa
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger  = Logger.getLogger(PersonServices.class.getName());
+    private Logger logger  = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
